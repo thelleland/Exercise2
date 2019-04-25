19ea9980b392a906765cedd8f1839f7d149cd20c
Tor Helleland <thelleland@hotmail.no>
2019-04-25 16:58:10 +0200

    Add files via upload

--- yolo.py
+++ yolo.py
@@ -186,48 +186,48 @@ model = Model([input_image, true_boxes], output)
 
 model.summary()
 
-# weight_reader = WeightReader(wt_path)
+weight_reader = WeightReader(wt_path)
 
-# weight_reader.reset()
-# nb_conv = 23
+weight_reader.reset()
+nb_conv = 23
 
-# for i in range(1, nb_conv+1):
-#     conv_layer = model.get_layer('conv_' + str(i))
+for i in range(1, nb_conv+1):
+    conv_layer = model.get_layer('conv_' + str(i))
     
-#     if i < nb_conv:
-#         norm_layer = model.get_layer('norm_' + str(i))
+    if i < nb_conv:
+        norm_layer = model.get_layer('norm_' + str(i))
         
-#         size = np.prod(norm_layer.get_weights()[0].shape)
+        size = np.prod(norm_layer.get_weights()[0].shape)
 
-#         beta  = weight_reader.read_bytes(size)
-#         gamma = weight_reader.read_bytes(size)
-#         mean  = weight_reader.read_bytes(size)
-#         var   = weight_reader.read_bytes(size)
+        beta  = weight_reader.read_bytes(size)
+        gamma = weight_reader.read_bytes(size)
+        mean  = weight_reader.read_bytes(size)
+        var   = weight_reader.read_bytes(size)
 
-#         weights = norm_layer.set_weights([gamma, beta, mean, var])       
+        weights = norm_layer.set_weights([gamma, beta, mean, var])       
         
-#     if len(conv_layer.get_weights()) > 1:
-#         bias   = weight_reader.read_bytes(np.prod(conv_layer.get_weights()[1].shape))
-#         kernel = weight_reader.read_bytes(np.prod(conv_layer.get_weights()[0].shape))
-#         kernel = kernel.reshape(list(reversed(conv_layer.get_weights()[0].shape)))
-#         kernel = kernel.transpose([2,3,1,0])
-#         conv_layer.set_weights([kernel, bias])
-#     else:
-#         kernel = weight_reader.read_bytes(np.prod(conv_layer.get_weights()[0].shape))
-#         kernel = kernel.reshape(list(reversed(conv_layer.get_weights()[0].shape)))
-#         kernel = kernel.transpose([2,3,1,0])
-#         conv_layer.set_weights([kernel])
+    if len(conv_layer.get_weights()) > 1:
+        bias   = weight_reader.read_bytes(np.prod(conv_layer.get_weights()[1].shape))
+        kernel = weight_reader.read_bytes(np.prod(conv_layer.get_weights()[0].shape))
+        kernel = kernel.reshape(list(reversed(conv_layer.get_weights()[0].shape)))
+        kernel = kernel.transpose([2,3,1,0])
+        conv_layer.set_weights([kernel, bias])
+    else:
+        kernel = weight_reader.read_bytes(np.prod(conv_layer.get_weights()[0].shape))
+        kernel = kernel.reshape(list(reversed(conv_layer.get_weights()[0].shape)))
+        kernel = kernel.transpose([2,3,1,0])
+        conv_layer.set_weights([kernel])
 
 
-# layer   = model.layers[-4] # the last convolutional layer
-# weights = layer.get_weights()
+layer   = model.layers[-4] # the last convolutional layer
+weights = layer.get_weights()
 
-# new_kernel = np.random.normal(size=weights[0].shape)/(GRID_H*GRID_W)
-# new_bias   = np.random.normal(size=weights[1].shape)/(GRID_H*GRID_W)
+new_kernel = np.random.normal(size=weights[0].shape)/(GRID_H*GRID_W)
+new_bias   = np.random.normal(size=weights[1].shape)/(GRID_H*GRID_W)
 
-# layer.set_weights([new_kernel, new_bias])
+layer.set_weights([new_kernel, new_bias])
 
-model.load_weights("./weights/five_anchors.h5")
+
 
 def custom_loss(y_true, y_pred):
     mask_shape = tf.shape(y_true)[:4]
